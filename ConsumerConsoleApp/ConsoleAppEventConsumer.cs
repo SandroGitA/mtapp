c8a0bdc1c54d989327936223bfad0528b6dd24ea
SandroGitA <maktavish74@mail.ru>
2024-11-05 12:17:25 +0500

    add comments

--- ConsumerConsoleApp/ConsoleAppEventConsumer.cs
+++ ConsumerConsoleApp/ConsoleAppEventConsumer.cs
@@ -6,12 +6,13 @@ namespace ConsumerConsoleApp
 {
     public class ConsoleAppEventConsumer : IConsumer<ConsoleAppEvent>
     {
+        //Подписываемся на сообщения типа ConsoleAppEvent
         public Task Consume(ConsumeContext<ConsoleAppEvent> context)
         {
             var jsonMessage = JsonConvert.SerializeObject(context.Message);
             Console.WriteLine($"EVENT_C1:" + jsonMessage);
 
-            Task.Delay(1000);
+            Task.Delay(10000);
 
             return Task.CompletedTask;
         }
--- ConsumerConsoleAppDouble/ConsoleAppDoubleEventConsumer.cs
+++ ConsumerConsoleAppDouble/ConsoleAppDoubleEventConsumer.cs
@@ -6,12 +6,13 @@ namespace ConsumerConsoleAppDouble
 {
     public class ConsoleAppDoubleEventConsumer : IConsumer<ConsoleAppEvent>
     {
+        //Подписываемся на такое же сообщение что и оригинальный подписчик ConsoleAppEvent
         public Task Consume(ConsumeContext<ConsoleAppEvent> context)
         {
             var jsonMessage = JsonConvert.SerializeObject(context.Message);
             Console.WriteLine($"EVENT_C2:" + jsonMessage);
 
-            Task.Delay(3000);
+            Task.Delay(30000);
 
             return Task.CompletedTask;
         }
