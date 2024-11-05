a6731023d82131a0c715dde8f46511133e179fdb
SandroGitA <maktavish74@mail.ru>
2024-11-05 19:03:28 +0500

    add new project

---
+++ ConsumerConsoleAppCommand/ConsoleAppCommandConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using MassTransitApp.Messages;
+using Newtonsoft.Json;
+
+namespace ConsumerConsoleAppCommand
+{
+    public class ConsoleAppCommandConsumer : IConsumer<CreateCarCommand>
+    {
+        public Task Consume(ConsumeContext<CreateCarCommand> context)
+        {
+            var jsonMessage = JsonConvert.SerializeObject(context.Message);
+            Console.WriteLine($"COMMAND_C1:" + jsonMessage);
+
+            Task.Delay(10000);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsumerConsoleAppCommand/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsumerConsoleAppCommand;
+using MassTransit;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+        {
+            cfg.ReceiveEndpoint("car-endpoint-command", e =>
+            {
+                e.Consumer<ConsoleAppCommandConsumer>();
+            });
+
+            cfg.Host("localhost", "/", srv =>
+            {
+                srv.Username("guest");
+                srv.Password("guest");
+            });
+        });
+
+        bus.Start();
+
+        Console.WriteLine("Start Console App Command...");
+        Console.ReadLine();
+    }
+}
